28a217abc3d326a526d3bd7b5f3723d00993c8da
码农小易 <237972113@qq.com>
2022-05-01 17:36:14 +0800

    vite.config.js修改为根据运行环境设置base路径

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,27 @@
 import { fileURLToPath, URL } from 'url'
 import styleImport, { VantResolve } from 'vite-plugin-style-import';
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { svgBuilder } from './src/plugins/svgBuilder'; 
+import { svgBuilder } from './src/plugins/svgBuilder';
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/vue-mobile-template/',
-  plugins: [
-    vue(),
-    styleImport({
-      resolves: [VantResolve()],
-    }),
-    svgBuilder('./src/assets/icons/svg/')
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode, command }) => {
+  const env = loadEnv(mode, process.cwd())
+  const { VITE_APP_ENV } = env
+  return {
+    base: VITE_APP_ENV === 'production' ? '/vue-mobile-template/' : '/',
+    plugins: [
+      vue(),
+      styleImport({
+        resolves: [VantResolve()],
+      }),
+      svgBuilder('./src/assets/icons/svg/')
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
